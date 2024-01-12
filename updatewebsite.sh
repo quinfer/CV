6691c266d2c55f38b6b9b9596fd4bf62556b75fa
Barry <b.quinn@qub.ac.uk>
2024-01-12 16:11:25 +0000

    adding new publication

--- updatewebsite.sh
+++ updatewebsite.sh
@@ -4,7 +4,7 @@ git remote set-url origin git+ssh://git@github.com/username/reponame.git
 git config --global user.name Barry Quinn
 git config --global user.email b.quinn@qub.ac.uk
 git config --global github.user quinfer
-git config --global github.token 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
+git config --global github.token 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
 git add .
 git commit -m "Add CV"
 git push
